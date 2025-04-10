bb0ff0ef34e0c4978ed9d9bca147ab2aea1a07be
zouazrou <183734171+zouazrou@users.noreply.github.com>
2025-04-10 14:05:36 +0100

    codingame

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include "Philosophers.h"
 
 
-void    thinking(philo_t *philo)
+void	sleeping(data_t *data, int id)
 {
-
-	// pthread_mutex_lock(&((data_t *)philo->data)->write);
-	printf("%lld %d is thinking\n", get_time_difference((data_t *)philo->data), philo->id);
-	// pthread_mutex_unlock(&((data_t *)philo->data)->write);
-	usleep(((data_t *)philo->data)->t_sleep);
+	pthread_mutex_lock(&data->write);
+	printf("%lld %d is sleeping\n", get_time_difference(data), id);
+	pthread_mutex_unlock(&data->write);
+	usleep(data->t_sleep);
+	pthread_mutex_lock(&data->write);
+	printf("%lld %d is thinking\n", get_time_difference(data), id);
+	pthread_mutex_unlock(&data->write);
 }
 
 // void    sleeping(data_t *data, int id)
@@ -44,37 +46,35 @@ void    *philosopher_life(void *arg)
 
 	philo = (philo_t *)arg;
 	data = (data_t *)philo->data;
-	pthread_mutex_lock(&data->write);
-	printf("test %d | time %lld\n", philo->id, get_time_difference(data));
-	pthread_mutex_unlock(&data->write);
 
-	// while (!data->simulation_stop)
-	// {
-	// 	eating(philo);
-	// 	// thinking(philo);
-	// }
+	pthread_mutex_lock(&data->write);
+	printf("%lld %d is thinking\n", get_time_difference(data), philo->id);
+	pthread_mutex_unlock(&data->write);
+	sleeping(data, philo->id);
 	// pthread_mutex_lock(&data->write);
+	// printf("before sleep in thread %d\n", philo->id);
 	// pthread_mutex_unlock(&data->write);
+	// sleep(1);
+	// pthread_mutex_lock(&data->write);
+	// printf("after sleep in thread %d\n", philo->id);
+	// pthread_mutex_unlock(&data->write);
+
 	return (NULL);
 }
 
-int main(int ac, char *av[])
+int	main(int ac, char *av[])
 {
-	data_t  data;
+	data_t	data;
 	int		i;
 
 	handling_input(&data, ac, av);
 	init_arg(&data);
 	i = -1;
 	// printf("time before create threads -> %lld-%lld\n", get_current_time() - data.start_time, get_current_time());
+	data.start_time = get_current_time();
 	while (++i < data.num_ph)
 	{
-		if (!i)
-			data.start_time = get_current_time();
 		pthread_create(&data.ph[i].th, NULL, philosopher_life, &data.ph[i]);
-		// pthread_mutex_lock(&data.write);
-		// printf("before join id = %lu\n", data.ph[i].th);
-		// pthread_mutex_unlock(&data.write);
 	}
 	data.simulation_stop = true;
 	i = -1;
